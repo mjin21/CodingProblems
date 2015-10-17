f0f0753b9264c4a660a315ee70b22fe770f1f0ae
Michael Jin <mjin21@gmail.com>
2015-10-17 12:25:31 -0700

    More problems

---
+++ CodingProblems/HourMinuteHand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+    class HourMinuteHand
+    {
+        public void CalculateAngle(DateTime time)
+        {
+            int minute = time.Minute;
+            int hour = time.Hour;
+
+            int hourAngle = (hour % 12) * (1 / 12) * 360 + (minute * 1 / 60 * 1/12 * 360);
+            int minuteAngle = minute * 1 / 60 * 360;
+
+            int difference = Math.Abs(hourAngle - minuteAngle);
+        }
+    }
+}
---
+++ CodingProblems/ReversePolish.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+    class ReversePolish
+    {
+        public int Calculate(char[] values)
+        {
+            Dictionary<char, bool> operations = new Dictionary<char, bool>();
+            var numbers = new Stack<int>();
+
+            for(int i = 0; i < values.Length; i++)
+            {
+                if (!operations.ContainsKey(values[i]))
+                    numbers.Push(values[i]);
+                else
+                {
+                    int a = numbers.Pop();
+                    int b = numbers.Pop();
+                    int calculated = Operate(b, a, values[i]);
+                 
+                    numbers.Push(calculated);
+                }
+            }
+            return numbers.Pop();
+        }
+
+        public int Operate(int a, int b, char operation)
+        {
+            int value = 0;
+
+            if(operation == '*')
+                value = a * b;
+            else if (operation == '+')
+                value = a + b;
+            else if (operation == '-')
+                value = a - b;
+            else if (operation == '/')
+                value = a / b;
+
+            return value;
+        }
+    }
+}

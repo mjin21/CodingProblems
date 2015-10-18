debc03e97b7b7e0582d3c7fbcad50b523634a631
Michael Jin <mjin21@gmail.com>
2015-10-17 17:20:43 -0700

    static readonly :p

--- CodingProblems/ReversePolish.cs
+++ CodingProblems/ReversePolish.cs
@@ -8,14 +8,15 @@ namespace CodingProblems
 {
     class ReversePolish
     {
+        private static readonly Dictionary<char, bool> _Operations = new Dictionary<char, bool>() { { '+', true }, { '-', true}, { '*', true }, { '/', true } };
+
         public int Calculate(char[] values)
         {
-            Dictionary<char, bool> operations = new Dictionary<char, bool>();
             var numbers = new Stack<int>();
 
             for(int i = 0; i < values.Length; i++)
             {
-                if (!operations.ContainsKey(values[i]))
+                if (!_Operations.ContainsKey(values[i]))
                     numbers.Push(values[i]);
                 else
                 {

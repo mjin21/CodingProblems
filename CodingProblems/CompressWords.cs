9d2e1d0fe6a3aea6ef2fb22d4d2b571034146078
Michael Jin <mjin21@gmail.com>
2015-10-26 10:54:01 -0700

    Updoots

---
+++ CodingProblems/CompressWords.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingProblems
+{
+    class CompressWords
+    {
+        public string Compress(string a)
+        {
+            string ret = "";
+            for(int i = 0; i < a.Length; i++)
+            {
+                int j = i + 1;
+                int count = 1;
+                while(a[i] == a[j])
+                {
+                    count++;
+                    j++;
+                }
+
+                if (count > 1)
+                    ret += a[i] + count;
+                else
+                    ret += a[i];
+            }
+
+            return a;
+        }
+
+        public string CompressAlt(string a)
+        {
+            string ret = "";
+            char last = a[0];
+
+            int count = 1;
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] == last)
+                   count++;
+                else
+                {
+                    ret += last + count;
+                    last = a[i];
+                    count = 1;
+                }
+            }
+
+            return a;
+        }
+    }
+}
--- CodingProblems/Program.cs
+++ CodingProblems/Program.cs
@@ -12,15 +12,6 @@ namespace CodingProblems
     {
         static void Main(string[] args)
         {
-            Trie trie = new Trie();
-
-            var words = File.ReadAllLines(@"C:\Users\Mike\Desktop\EnglishWords.txt");
-            foreach (string word in words)
-                trie.Insert(word);
-
-            var found1 = trie.GetMatches("are");
-            var found2 = trie.GetMatches("ara");
-            var found3 = trie.GetMatches("cae");
         }
     }
 }
